bbf5c53518ddea342349eb65ef4c61252584750d
itscholey <chloebarnes3@gmail.com>
2017-03-21 08:03:36 +0000

    Added expiry again.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_fyp-site_session'
+Rails.application.config.session_store :cookie_store,
+key: '_fyp-site_session',
+:expire_after => 60.minutes
