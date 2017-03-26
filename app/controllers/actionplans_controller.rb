4648b76342fd93a0263e685fbde51c3d83a4d649
itscholey <chloebarnes3@gmail.com>
2017-03-26 20:41:11 +0100

    Added controller for Actionitem.

--- app/controllers/actionplans_controller.rb
+++ app/controllers/actionplans_controller.rb
@@ -1,10 +1,31 @@
 class ActionplansController < ApplicationController
+  def index
+    @actionplans = Actionplan.all
+  end
+
   def new
+    @actionplan = Actionplan.new
+  end
+
+  def create
+    @actionplan = Actionplan.new(actionplan_params)
+    if @actionplan.save
+      render :show
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def show
+    @actionplan = Actionplan.find(params[:id])
+  end
+
+  private
+
+  def actionplan_params
+    params.require(:actionitems).permit(:title, :description, :student_id, :supervisor_id)
   end
 end
